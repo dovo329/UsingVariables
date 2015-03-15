540b0dee7047da1ab4117bd39a371173baede9c2
Doug Voss <douglas.c.voss@gmail.com>
2015-03-14 21:21:51 -0600

    added BMI calculation code and printout

--- UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
+++ UsingVariables-Objc/UsingVariables-Objc/AppDelegate.m
@@ -18,6 +18,24 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
     // Insert code here...
+    CGFloat height;
+    CGFloat weight;
+    
+    height = 71.0f;
+    weight = 185.0f;
+    
+    CGFloat bodyMassIndex = (weight / (height * height)) * 703;
+    
+    NSInteger intBMI = (NSInteger)bodyMassIndex;
+    NSInteger roundBMI = roundf(bodyMassIndex);
+    
+    NSLog(@"%f",bodyMassIndex);
+    //NSLog(@"%f %d %d",bodyMassIndex, intBMI, roundBMI);
+    
+    //intBMI++;
+    //roundBMI -= 5;
+    
+    //NSLog(@"%f %d %d",bodyMassIndex, intBMI, roundBMI);
     
     
     return YES;
