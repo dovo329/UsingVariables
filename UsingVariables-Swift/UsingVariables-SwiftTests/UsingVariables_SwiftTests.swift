1c5eff6bd0d79acef57668c5ce94a4a091f3390e
Doug Voss <douglas.c.voss@gmail.com>
2015-04-13 12:04:01 -0600

    added xcode 6.3 version for swift

--- UsingVariables-Swift/UsingVariables-SwiftTests/UsingVariables_SwiftTests.swift
+++ UsingVariables-Swift/UsingVariables-SwiftTests/UsingVariables_SwiftTests.swift
@@ -2,8 +2,8 @@
 //  UsingVariables_SwiftTests.swift
 //  UsingVariables-SwiftTests
 //
-//  Created by Joshua Howland on 8/14/14.
-//  Copyright (c) 2014 DevMountain. All rights reserved.
+//  Created by Douglas Voss on 4/13/15.
+//  Copyright (c) 2015 Doug. All rights reserved.
 //
 
 import UIKit
